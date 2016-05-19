1ce99c3d16d2db780086c0461456f82927a4642a
codingCPA <matthew.a.berard@gmail.com>
2016-05-19 13:25:46 -0400

    To update pangram.rb to pass all tests

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,7 +1,13 @@
 class Pangram
   def self.is_pangram?(str)
-    false
-
+    return false if str.length < 26
+    hash = {}
+    str.downcase.each_char do |x|
+      hash[x] = true if ('a'..'z').include?(x)# x is not in the alphabet
+    end
+    hash.keys.count == 26
   end
+
+VERSION = 1
 
 end
